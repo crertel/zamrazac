73ec3a440e60f6e016e6599e25045cf5ad66bdf2
crertel <crertel@mashengo.localdomain>
2019-05-16 16:17:05 -0500

    Initial commit.

---
+++ lib/zamrazac.ex
@@ -0,0 +1,18 @@
+defmodule Zamrazac do
+  @moduledoc """
+  Documentation for Zamrazac.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Zamrazac.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zamrazac_test.exs
@@ -0,0 +1,8 @@
+defmodule ZamrazacTest do
+  use ExUnit.Case
+  doctest Zamrazac
+
+  test "greets the world" do
+    assert Zamrazac.hello() == :world
+  end
+end
