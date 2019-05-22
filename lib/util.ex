21564b48844ffe9ad51c60a5fa755d0cbb44ae24
Chris Ertel <chris@kedagital.com>
2019-05-22 15:21:34 -0400

    Add directory utils.

--- lib/util.ex
+++ lib/util.ex
@@ -1,15 +1,30 @@
 defmodule Zamrazac.Util do
   @moduledoc """
-  Module hold kitchen-sink functions.
+  Module to hold kitchen-sink functions.
   """
 
-
-
+  @doc """
+  get_blog_directory will return the directory where blog files are stored.
+  First check the value of $BLOG_DIRECTORY in the environment, otherwise default to ./blog
+  """
   def get_blog_directory() do
-
+    (System.get_env("BLOG_DIRECTORY") || get_default_blog_directory())
+    |> Path.expand()
   end
 
-  def get_blog_image_directory(), do: Path.join(get_blog_directory(), "images")
+  @doc """
+  Get the default blog directory, which is typically off of the zamrazac directory.
+  """
+  def get_default_blog_directory() do
+    __ENV__.file
+    |> Path.dirname()
+    |> Path.join( "../blog")
+    |> Path.expand()
+  end
 
+  @doc """
+  Gets the image directory for the blog, using either the default or environment-defined directory.
+  """
+  def get_blog_image_directory(), do: Path.join(get_blog_directory(), "images")
 
 end
