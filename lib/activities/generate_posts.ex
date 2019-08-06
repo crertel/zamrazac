9549e9c50d5c07a07e0f5f195991567b347059d2
crertel <chris@kedagital.com>
2019-08-05 20:36:16 -0400

    Make mobile render better.

--- lib/activities/generate_posts.ex
+++ lib/activities/generate_posts.ex
@@ -207,10 +207,11 @@ defmodule Zamrazac.Activities.GeneratePosts do
         <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
         <meta http-equiv="Pragma" content="no-cache" />
         <meta http-equiv="Expires" content="0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><%= post_title %></title>
         <style>
         .post-main {
-          width: 960px;
+          max-width: 960px;
           margin: auto;
         }
 
@@ -268,10 +269,11 @@ defmodule Zamrazac.Activities.GeneratePosts do
         <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
         <meta http-equiv="Pragma" content="no-cache" />
         <meta http-equiv="Expires" content="0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><%= blog_title %></title>
         <style>
         .post-main {
-          width: 960px;
+          max-width: 960px;
           margin: auto;
         }
 
