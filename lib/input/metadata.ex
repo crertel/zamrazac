9bc0784e8123c6b6afc79217527512ca92bf5e7d
crertel <chris@kedagital.com>
2022-04-23 17:54:50 -0500

    Add missing fields to metadata.

--- lib/input/metadata.ex
+++ lib/input/metadata.ex
@@ -1,5 +1,4 @@
 defmodule Zamrazac.Input.Metadata do
-
   defstruct title: nil,
             author: nil,
             date: nil,
@@ -7,5 +6,9 @@ defmodule Zamrazac.Input.Metadata do
             filename: nil,
             basename: nil,
             slug: nil,
-            series: nil
+            series: nil,
+            next_post_title: nil,
+            next_post_path: nil,
+            prev_post_title: nil,
+            prev_post_path: nil
 end
